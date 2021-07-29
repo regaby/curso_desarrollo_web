95e64e304541b57abc228a55bece476edc80d419
Gabriela Rivero <regaby@gmail.com>
2021-07-29 15:38:23 -0300

    [UPD] preparando clase 9

--- curso_desarrollo_web/settings.py
+++ curso_desarrollo_web/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'desafios',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'curso_desarrollo_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            #BASE_DIR / "desafios" / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- desafios/views.py
+++ desafios/views.py
@@ -2,6 +2,7 @@ from django.http.response import HttpResponse, HttpResponseNotFound, HttpRespons
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -24,11 +25,12 @@ def index(request):
     #return HttpResponse("""<h1>App desafios</h1>""")
     lista_meses = list(desafios.keys())
     print ('lista_meses', lista_meses)
-    meses = ''
-    for mes in lista_meses:
-        meses += f'<li><a href="{mes}">{mes}</a></li>'
-    print ('meses', meses)
-    return HttpResponse(f'<ul>{meses}</ul>')
+    # meses = ''
+    # for mes in lista_meses:
+    #     meses += f'<li><a href="{mes}">{mes}</a></li>'
+    # print ('meses', meses)
+    # return HttpResponse(f'<ul>{meses}</ul>')
+    return render(request, "desafios/index.html",{'lista_meses': lista_meses})
 
 def desafio_mensual_por_numero(request, mes):
     lista_meses = list(desafios.keys())
@@ -41,7 +43,9 @@ def desafio_mensual(request, mes):
     print ('mes', mes)
     texto_desafio = ''
     try:
-        texto_desafio = desafios[mes]
+        #texto_desafio = desafios[mes]
+        #texto_desafio = render_to_string("desafios/desafio.html")
+        return render(request, "desafios/desafio.html", {'texto': desafios[mes],'mes': mes})
     except:
         return HttpResponseNotFound('<h2>mes no implementado<h2>')
     return HttpResponse(f'<h1>{texto_desafio}</h1>')
